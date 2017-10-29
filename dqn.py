b7068fd612931c5a0e05d56e82e8b5931e240f0f
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-10-29 17:57:35 +0100

    added more functions and parameters

--- dqn.py
+++ dqn.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+from collections import deque
+import random
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Convolution2D, Flatten
 from keras.optimizers import Adam
@@ -21,14 +23,21 @@ class DQN():
         self.frame_height = frame_height
         self.state_length = state_length
         self.num_actions = num_actions
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.s, self.q_values, self.model = self.build_network()
 
-    def preprocess(observation, last_observation):
+    def preprocess(self, observation, last_observation):
         processed_observation = np.maximum(observation, last_observation)
         processed_observation = np.uint8(
             resize(rgb2gray(processed_observation), (self.frame_width, self.frame_height)) * 255)
         return np.reshape(processed_observation, (1, self.frame_width, self.frame_height))
 
-    def network():
+    def build_network(self):
         model = Sequential()
         model.add(Convolution2D(32, 8, 8, subsample=(4, 4), activation='relu',
                                 input_shape=(self.state_length, self.frame_width, self.frame_height)))
@@ -43,6 +52,15 @@ class DQN():
         q_values = model(s)
 
         return s, q_values, model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
 
 
 def main():
